328fa1483081d62c864680093bd04407855ca411
DavidLeirado <dleiradomaroto@gmail.com>
2021-05-08 19:35:17 +0200

    definido metodo para extraer las URN de cada texto. Corregidas las variables del constructor para atacar la API

--- scraper/perseus-scraper.py
+++ scraper/perseus-scraper.py
@@ -5,11 +5,29 @@ import sys
 import bs4
 import requests
 import logging
+import time
 
+# Iniciamos la clase Perseus()
 class Perseus():
+    # Iniciamos el constructor de la clase Perseus()
     def __init__(self):
-        self.__perseusAPI = https://scaife.perseus.org/library/
-        self.__greekMarker = 'urn:cts:greekLit:'
+        self.__perseusAPI_capabilities = 'http://www.perseus.tufts.edu/hopper/CTS?request=GetCapabilities'
+        self.__greekMarker = 'greekLit:'
+
+    urn_codes = []
+
+    # Al llamar a este método, se recopilan todos los códigos URN correspondientes a textos en griego
+    def get_urns(self):
+        persAPIcap_req = requests.get(self.__perseusAPI_capabilities)
+        persAPIcap_parser = bs4.BeautifulSoup(persAPIcap_req.text, 'xml')
+        
+        for textgroup in persAPIcap_parser.find_all('textgroup'):
+            if self.__greekMarker in textgroup['projid']:
+                for work in textgroup.find_all('work'):
+                    logging.debug(work['urn'])
+                    self.urn_codes.append(work['urn'])
+        logging.debug('La cantidad de textos a scrapear es de: '+str(len(self.urn_codes)))
+
 
 
 
@@ -21,3 +39,6 @@ if __name__ == "__main__":
     if not 'DEBUG' in sys.argv:
         logging.disable(logging.DEBUG)
     logging.debug('Inicio del programa')
+
+    perseusScrap = Perseus()
+    perseusScrap.get_urns()
