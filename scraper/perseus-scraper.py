f8e1a0b8e07d34c68891fd2ac7523880e04926e0
DavidLeirado <dleiradomaroto@gmail.com>
2021-05-08 17:46:02 +0200

    Configurado modo DEBUG para el scraper

--- scraper/perseus-scraper.py
+++ scraper/perseus-scraper.py
@@ -4,4 +4,14 @@ import os
 import sys
 import bs4
 import requests
-import logger
+import logging
+
+
+if __name__ == "__main__":
+    
+    # Configuración de modo DEBUG por parámetro
+    logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s- %(message)s')
+    
+    if not 'DEBUG' in sys.argv:
+        logging.disable(logging.DEBUG)
+    logging.debug('Inicio del programa')
