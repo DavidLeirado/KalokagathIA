01e44f03104e36f407818bd42b0050d55f34d7eb
DavidLeirado <dleirado@hotmail.com>
2021-05-20 20:59:15 +0200

    Creada clase padre para sacar textos concretos

--- dataset-ampliaciones-xml/ampliacion-aristoteles.py
+++ dataset-ampliaciones-xml/ampliacion-aristoteles.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 
 import os, sys, bs4
+
+count=0
+for autor in os.listdir('Classics/'):
+
+    try:
+        for xml in os.listdir('Classics/'+autor+'/opensource/'):
+            if '_gk' in xml:
+                with open('Classics/'+autor+'/opensource/'+xml, 'r', encoding='utf-8') as rawtext:
+                    
+                    text = bs4.BeautifulSoup(rawtext, 'xml')
+                    print(text.get_text())
+                    count+=1
+        
+    except FileNotFoundError:
+        print('El directorio '+xml+' está vacío')
+
+print(count)
